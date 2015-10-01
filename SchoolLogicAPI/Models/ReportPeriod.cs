8888164aecf182fc9dea58c48515f056cfe36cb2
Mark Strendin <mark.strendin@lskysd.ca>
2015-10-01 12:58:02 -0600

    Added report period model

---
+++ SchoolLogicAPI/Models/ReportPeriod.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolLogicAPI.Models
+{
+    public class ReportPeriod
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int ID { get; set; }
+        public int TermId { get; set; }
+        public string Name { get; set; }
+        public int SchoolGovId { get; set; }
+        public int SchoolInternalId { get; set; }
+        public int DaysOpenBeforeEnd { get; set; }
+        public int DaysOpenAfterEnd { get; set; }
+        public DateTime DateOpens
+        {
+            get
+            {
+                return EndDate.AddDays(this.DaysOpenBeforeEnd * -1);
+            }
+        }
+        public DateTime DateCloses
+        {
+            get
+            {
+                return EndDate.AddDays(this.DaysOpenAfterEnd);
+            }
+        }
+
+        // Parameterless constructor for serialization
+        public ReportPeriod() { }
+    }
+}
